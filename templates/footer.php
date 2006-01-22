f552437848cb323ca70058332648e0c5fef27e07
Shish <webmaster@shishnet.org>
2006-01-22 14:37:14 +0000

    tracacacacacakekekekeke

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@ echo <<<EOD
 		<div id="footer">
 			<hr>
 			Images &copy; their respective owners,
-			<a href="http://shish.is-a-geek.net/projects/shimmie/about.php">Shimmie</a> &copy; 
+			<a href="http://shish.is-a-geek.net/trac/shimmie/">Shimmie</a> &copy; 
 			<a href="http://shish.is-a-geek.net/">Shish</a> 2005,
 			based on the <a href="http://danbooru.donmai.us/">Danbooru</a> concept.
 		</div>
