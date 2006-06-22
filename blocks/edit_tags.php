17603bbf106d7f6ee7d7852b594190e85d62f1d0
Shish <webmaster@shishnet.org>
2006-06-22 16:15:37 +0000

    people are dumb -_-

--- blocks/edit_tags.php
+++ blocks/edit_tags.php
@@ -9,9 +9,27 @@ if($pageType == "view") {
 	global $image;
 
 	$blocks[20] .= <<<EOD
+	<script language="javascript">
+		function tagEditConfirm() {
+			if(getCookie("I_know_that_edit_is_not_search")) {
+				confirmed = true;
+			}
+			else {
+				confirmed = confirm(""+
+					"This is a tag edit box.\\n"+
+					"This is not a search box.\\n"+
+					"Do you *really* want to edit the tags?"+
+					"");
+				if(confirm("Can you remember that the thing labelled\\n'edit tags' is not a search box?")) {
+					setCookie("I_know_that_edit_is_not_search", true);
+				}
+			}
+			return confirmed;
+		}
+	</script>
 	<h3 onclick="toggle('tags')">Edit Tags</h3>
 	<div id="tags">
-		<form action="metablock.php?block=edit_tags" method="POST">
+		<form onSubmit="return tagEditConfirm();" action="metablock.php?block=edit_tags" method="POST">
 			<input name="image_id" type="hidden" value="$image->id">
 			<input name="tags" type="text" value="$image->tags">
 			<!-- <input type="submit" value="Set"> -->
