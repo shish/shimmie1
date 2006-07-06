aa665c62ee7a5055d94d8eb7e35d247ffc61ff27
Shish <webmaster@shishnet.org>
2006-07-06 04:56:40 +0000

    moved trac

--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,8 @@ echo <<<EOD
 		<div id="footer">
 			<hr>
 			Images &copy; their respective owners,
-			<a href="http://shish.is-a-geek.net/trac/shimmie/">Shimmie</a> &copy; 
-			<a href="http://shish.is-a-geek.net/">Shish</a> 2005,
+			<a href="http://trac.shishnet.org/shimmie/">Shimmie</a> &copy; 
+			<a href="http://www.shishnet.org/">Shish</a> 2005,
 			based on the <a href="http://danbooru.donmai.us/">Danbooru</a> concept.
 		</div>
 	</body>
