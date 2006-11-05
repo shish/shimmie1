cfd1465ab98c88c4cce4ee5ee6f74a95cd0de425
Shish <webmaster@shishnet.org>
2006-11-05 21:08:12 +0000

    tag search explanation

--- index.php
+++ index.php
@@ -43,6 +43,29 @@ $start = $cpage * $imagesPerPage;
  * and an unlimited one to count, we could just do the unlimited
  * one and put the limit in PHP, but that's still not as neat as
  * doing a limited query and a separate count()...
+
+  searched for, score = 1; if the tag is one the user has blocked,
+  score = -1. so the user searches for "tag1 not tag2", which gives us:
+  objectName,tagValue,score
+  object1,tag1,1
+  object1,tag2,-1
+  object2,tag1,1
+
+  then we group the rows by objectName, and sum the score column:
+  objectName,tagValue,score
+  object1,tag1,0
+  object2,tag1,1
+
+  then do "having score = $number of tags searched for" ("having"
+  works like "where", but it works with group columns, eg "sum" or "average"):
+  objectName,tagValue,score
+  object2,tag1,1
+
+  which then gives you what you searched for. You can also set the
+  threshold lower, eg show things that match at least one tag (having
+  score > 0), or you can combine that with the tag block with (if tag
+  = one blocked, score = -99999)
+  
  */
 $searchString = "";
 
