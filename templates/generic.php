306c057c59790570c63ae87bfeae3b691f27eac7
Shish <webmaster@shishnet.org>
2006-12-25 17:24:35 +0000

    now only one template file

--- templates/generic.php
+++ templates/generic.php
@@ -1,13 +1,16 @@
 <?php
-require_once "templates/header.php";
-
-if(!is_null($data)) {
-	$databox = "<p><textarea cols='80' rows='10'>$data</textarea>";
+if($baseurl) {
+	$base_html = "<base href='$baseurl'>";
 }
 
-if(!is_null($help)) {
-	$helpblock .= "<h3 onclick=\"toggle('help')\">Help</h3>\n";
-	$helpblock .= "<div id='help'>$help</div>\n";
+$scripts = glob("scripts/*.js");
+$script_html = "";
+foreach($scripts as $script) {
+	$script_html .= "\t\t<script src='$script' type='text/javascript'></script>\n";
+}
+
+if(!is_null($subtitle)) {
+	$subtitle_html = "<div id='subtitle'>$subtitle</div>";
 }
 
 if(!is_null($navigation)) {
@@ -15,21 +18,54 @@ if(!is_null($navigation)) {
 	$navblock .= "<div id='navigate'>$navigation</div>\n";
 }
 
+if(!is_null($help)) {
+	$helpblock .= "<h3 onclick=\"toggle('help')\">Help</h3>\n";
+	$helpblock .= "<div id='help'>$help</div>\n";
+}
+
+if(!is_null($data)) {
+	$databox = "<p><textarea cols='80' rows='10'>$data</textarea>";
+}
+
 if(is_null($heading)) $heading = $title;
 
+global $version;
+
 echo <<<EOD
-<div id="nav">
-	$blocks
-	$navblock
-	$helpblock
-</div>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+	<head>
+		$base_html
+		<title>$title</title>
+		<link rel="stylesheet" href="style.css" type="text/css">
+		$script_html
+		$extra_headers
+	</head>
 
-<div id="body">
-	<h3>$heading</h3>
-	$message
-	$databox
-</div>
+	<body>
+		<h1>$title</h1>
+		$subtitle_html
+		
+		<div id="nav">
+			$blocks
+			$navblock
+			$helpblock
+		</div>
+
+		<div id="body">
+			<h3>$heading</h3>
+			$message
+			$databox
+		</div>
+
+		<div id="footer">
+			<hr>
+			Images &copy; their respective owners,
+			<a href="http://trac.shishnet.org/shimmie/">$version</a> &copy; 
+			<a href="http://www.shishnet.org/">Shish</a> 2005 - 2006,
+			based on the <a href="http://danbooru.donmai.us/">Danbooru</a> concept.
+		</div>
+	</body>
+</html>
 EOD;
-
-require_once "templates/footer.php";
 ?>
