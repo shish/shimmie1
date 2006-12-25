a3f020c19363c48099cdf74d731379ffc261edd4
Shish <webmaster@shishnet.org>
2006-12-25 17:06:19 +0000

    another

--- view.php
+++ view.php
@@ -13,7 +13,17 @@ $dir_images = $config['dir_images'];
 $baseurl = $config['base_href'];
 $scale = $config['view_scale'] ? "style='width: 90%'" : "";
 
+if(!empty($image->slink)) {
+	$slink_html = "<br/>Short link: <input type='text' size='50' value='{$image->slink}'>";
+}
+
 $title = html_escape($image->tags);
 $blocks = get_blocks_html("view");
-require_once "templates/view.php";
+$heading = "Image";
+$message = "
+	<img onclick='scale(this)' src='{$image->link}' alt='{$image->tags}' $scale>
+	$slink_html
+	<br/>Uploaded by {$image->owner}
+";
+require_once "templates/generic.php";
 ?>
