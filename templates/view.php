54e569d91c96a454c6671df054b1c5b8ce75a1c9
Shish <webmaster@shishnet.org>
2006-12-25 16:46:37 +0000

    allow hiding short link

--- templates/view.php
+++ templates/view.php
@@ -1,5 +1,10 @@
 <?php
 require_once "templates/header.php";
+
+if(!empty($image->slink)) {
+	$slink_html = "<br/>Short link: <input type='text' size='50' value='{$image->slink}'>";
+}
+
 echo <<<EOD
 <div id="nav">$blocks</div>
 
@@ -7,7 +12,7 @@ echo <<<EOD
 	<h3>Image</h3>
 	<center>
 		<img onclick="scale(this)" src="$image->link" alt="$image->tags" $scale>
-		<br/>Short link: <input type="text" size="50" value="$baseurl$image->slink">
+		$slink_html
 		<br/>Uploaded by $image->owner
 	</center>
 </div>
